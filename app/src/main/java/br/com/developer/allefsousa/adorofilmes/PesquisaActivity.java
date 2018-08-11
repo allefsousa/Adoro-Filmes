479e38cb25a9b1cb16f27e8bc47d21cb65841749
Allef sousa <allefsousa_1@hotmail.com>
2018-08-11 17:19:01 -0300

    inicio projeto

---
+++ app/src/main/java/br/com/developer/allefsousa/adorofilmes/PesquisaActivity.java
@@ -0,0 +1,13 @@
+package br.com.developer.allefsousa.adorofilmes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PesquisaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesquisa);
+    }
+}
