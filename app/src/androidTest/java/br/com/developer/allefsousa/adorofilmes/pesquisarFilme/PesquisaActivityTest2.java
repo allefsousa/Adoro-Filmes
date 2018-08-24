520884025fcf8cfedf9a09380f0201839cddff68
Allef sousa <allefsousa_1@hotmail.com>
2018-08-24 16:15:55 -0300

    Teste falha

--- app/src/androidTest/java/br/com/developer/allefsousa/adorofilmes/pesquisarFilme/PesquisaActivityTest2.java
+++ app/src/androidTest/java/br/com/developer/allefsousa/adorofilmes/pesquisarFilme/PesquisaActivityTest2.java
@@ -83,8 +83,9 @@ public class PesquisaActivityTest2 {
 
 
         // matcher que compara com uma mensagem de toast
-//
-
+        onView(withText("Login successfully."))
+                .inRoot(withDecorView(not(activityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
     }
 
     public static Matcher withErrorEditext(final String expected) {
